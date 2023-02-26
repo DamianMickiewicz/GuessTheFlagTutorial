e62898bfd5cb68553e80f9276a19a61f3f30da45
DamianMickiewicz <d.mickiewicz@outlook.com>
2023-02-26 16:49:55 +0100

    add first tests

--- GuessTheFlagProject/GuessTheFlagProjectTests/GuessTheFlagProjectTests.swift
+++ GuessTheFlagProject/GuessTheFlagProjectTests/GuessTheFlagProjectTests.swift
@@ -9,7 +9,9 @@ import XCTest
 @testable import GuessTheFlagProject
 
 final class GuessTheFlagProjectTests: XCTestCase {
-
+    
+    let viewController = ViewController()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
@@ -18,15 +20,36 @@ final class GuessTheFlagProjectTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testViewControllerNil() throws {
+        XCTAssertNotNil(viewController, "ViewController is nil")
     }
 
+    func testIsCorrectAnswer() throws {
+        let buttonID = 0
+        let isCorrectAnswer = viewController.isCorrectAnswer(buttonId: buttonID)
+        let correctAnswer = viewController.correctAnswer
+        
+        XCTAssertTrue(isCorrectAnswer, "correctAnswer = \(correctAnswer), buttonID = \(buttonID)")
+    }
+    
+    func testIsNotCorrectAnswer() throws {
+        let buttonID = 2
+        let isCorrectAnswer = viewController.isCorrectAnswer(buttonId: buttonID)
+        let correctAnswer = viewController.correctAnswer
+        
+        XCTAssertFalse(isCorrectAnswer, "correctAnswer = \(correctAnswer), buttonID = \(buttonID)")
+    }
+    
+    func testIsEqualTrying() throws {
+        let correctAnswer = viewController.correctAnswer
+        let buttonID = 0
+        
+        XCTAssertEqual(correctAnswer, buttonID, "Isn't equal")
+    }
+    
+    
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
